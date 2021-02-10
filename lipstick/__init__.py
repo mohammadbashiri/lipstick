3845f054cb5a243f03fdbe7bdd894d3daf25db2d
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-02-10 23:10:29 +0100

    Fix missing imports and add init.py

---
+++ lipstick/__init__.py
@@ -0,0 +1,2 @@
+from .scatter_hist import scatter_hist
+from .scatter_diag import scatter_diag
--- lipstick/scatter_hist.py
+++ lipstick/scatter_hist.py
@@ -1,34 +1,41 @@
-def scatter_hist(x, y, 
-                 diag_on=False,
-                 x_hist_height=1.,
-                 y_hist_width=1.,
-                 fig_kwgs=None, 
-                 scatter_kwgs=None,
-                 diag_kwgs=None,
-                 hist_kwgs=None,
-                 binwidth=0.1):
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter_hist(
+    x,
+    y,
+    diag_on=False,
+    x_hist_height=1.0,
+    y_hist_width=1.0,
+    fig_kwgs=None,
+    scatter_kwgs=None,
+    diag_kwgs=None,
+    hist_kwgs=None,
+    binwidth=0.1,
+):
 
     from mpl_toolkits.axes_grid1 import make_axes_locatable
-    
+
     fig_kwgs = fig_kwgs if fig_kwgs is not None else {}
-    
+
     if fig_kwgs is not None:
         pass
     else:
-        fig_kwgs = {'figsize': (5.5, 5.5)}
-        
+        fig_kwgs = {"figsize": (5.5, 5.5)}
+
     fig, axScatter = plt.subplots(**fig_kwgs)
 
     # the scatter plot:
-    scatter_kwgs = scatter_kwgs if scatter_kwgs is not None else {'s':10, 'color': 'k'}
+    scatter_kwgs = scatter_kwgs if scatter_kwgs is not None else {"s": 10, "color": "k"}
     axScatter.scatter(x, y, **scatter_kwgs)
-    axScatter.set_aspect(1.)
-    
+    axScatter.set_aspect(1.0)
+
     if diag_on:
         min_lim = min(min(x), min(y))
         max_lim = max(max(x), max(y))
 
-        diag_kwgs = diag_kwgs if diag_kwgs is not None else {'c': '.4', 'ls': '--'}
+        diag_kwgs = diag_kwgs if diag_kwgs is not None else {"c": ".4", "ls": "--"}
         axScatter.plot([min_lim, max_lim], [min_lim, max_lim], max_lim, **diag_kwgs)
 
     # create new axes on the right and on the top of the current axes
@@ -41,23 +48,27 @@ def scatter_hist(x, y,
     # make some labels invisible
     axHistx.xaxis.set_tick_params(labelbottom=False)
     axHisty.yaxis.set_tick_params(labelleft=False)
-    
+
     if hist_kwgs is not None:
-        if 'bins' not in hist_kwgs.keys():
+        if "bins" not in hist_kwgs.keys():
             min_lim = min(min(x), min(y))
             max_lim = max(max(x), max(y))
             drange = max_lim - min_lim
-            bins = np.arange(min_lim - .1*drange, max_lim + .1*drange + binwidth, binwidth)
+            bins = np.arange(
+                min_lim - 0.1 * drange, max_lim + 0.1 * drange + binwidth, binwidth
+            )
     else:
         min_lim = min(min(x), min(y))
         max_lim = max(max(x), max(y))
         drange = max_lim - min_lim
-        bins = np.arange(min_lim - .1*drange, max_lim + .1*drange + binwidth, binwidth)
-        hist_kwgs = {'color': 'k'}
-        
+        bins = np.arange(
+            min_lim - 0.1 * drange, max_lim + 0.1 * drange + binwidth, binwidth
+        )
+        hist_kwgs = {"color": "k"}
+
         print(bins[:5])
-    
+
     axHistx.hist(x, bins=bins, **hist_kwgs)
-    axHisty.hist(y, bins=bins, orientation='horizontal', **hist_kwgs)
-    
+    axHisty.hist(y, bins=bins, orientation="horizontal", **hist_kwgs)
+
     return fig, axScatter, axHistx, axHisty
